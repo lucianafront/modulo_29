442fa25442d983caa3d41a54033253deca91aef9
Luciana de Souza Felix <luciana.quimicatec@outlook.com>
2024-01-22 19:56:51 -0300

    Exxercício-29

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,36 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'modulo_29';
+
+  
+  // lista original
+  carros = [
+    { id: 1, marca: 'Ford', modelo: 2006, color: 'Azul' },
+    { id: 2, marca: 'Chevete', modelo: 2023, color: 'Azul' },
+    { id: 3, marca: 'Ford', modelo: 2000, color: 'vermelho' },
+    { id: 4, marca: 'Vectra', modelo: 1980, color: 'verde' },
+    { id: 5, marca: 'Fusca', modelo: 1998, color: 'preto' },
+  ];
+
+  // lista de carros filtrados usando filter
+  carrosMarcaIniciadosComLetraF = this.carros.filter((carro) => carro.marca.startsWith('F'));
+  carrosAno = this.carros.filter((carro) => carro.modelo >= 2000);
+
+
+  // lista de carros modificados usando map
+  carrosModeloMais10 = this.carros.map((carro) => {
+    return { ...carro, modelo: carro.modelo + 50 };
+  });
+
+  carrosRosa = this.carros.map((carro) => {
+    return { ...carro, color: 'Rosa' };
+  });
+
+
+  // soma de valores usando reduce
+  somaValor = this.carros.reduce((total, carro) => total + carro.modelo, 0);
+
+
+
+ 
 }
